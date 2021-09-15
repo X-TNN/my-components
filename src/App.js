ecd441c078c37f42c92a3aada9c430a796f7b6c6
tnn <tiannn@bonree.com>
2021-09-15 10:44:01 +0800

    下拉框组件封装

--- src/App.js
+++ src/App.js
@@ -3,8 +3,47 @@ import 'antd/dist/antd.css'
 import './assets/font/style.css'
 import './assets/css/index.less';
 
-const datas = ['Jack', 'Lucy', 'Tom', 'Mike'];
+// const datas = [
+//   {value:'1',label:'Jack',disabled:true},
+//   {value:'2',label:'Lucy'},
+//   {value:'3',label:'Tom'},
+//   {value:'4',label:'Mike'},
+// ];
+const datas = [
+  {
+    label:'Manager',
+    key:'Manager',
+    options: [
+      {
+        value:'1',
+        label:'Jack',
+        disabled:'true'
+      },
+      {
+        value:'2',
+        label:'Lucy'
+      },
+      {
+        value:'4',
+        label:'Mike'
+      }
+    ]
+  },
+  {
+    label:'Engineer',
+    key:'Engineer',
+    options: [
+      {
+        value:'3',
+        label:'Tom'
+      }
+    ]
+  }
+];
 
+function onChange(value){
+  console.log('onChange:', value);
+}
 
 function App() {
   return (
@@ -12,8 +51,12 @@ function App() {
       <div className="select-box">
         <CustomSelect
           datas={datas}
+          //defaultValue={{value:2,label:'Lucy'}}
+          //priorOption={{value:-1,label:'全部'}} // 设置优先选项是否保留，总是在列表第一项显示并且作为默认值, 内容{label, value}
           placeholder='请选择'
+          onChange={onChange}
           searchable
+          loading={false}
         />
       </div>
     </div>
--- src/components/CustomSelect/CustomSelect.jsx
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -1,37 +1,99 @@
 import React,{useState} from "react";
-import { Select } from 'antd';
+import { Select, Spin  } from 'antd';
+//const { Option, OptGroup} = Select;
 
-const { Option } = Select;
 
-function onBlur() {
-  console.log('blur');
-}
+function CustomSelect(props) {
+    let optionValues = props.datas;  //定义存储原数据options的变量
+    const [userValue, setUserValue] = useState(''); //用户输入的值
+    const [userOption, setUserOption] = useState(); //用户选中的自定义option
+    const [defaultValue, setDefaultValue] = useState(getDefaultValue());  //默认选项
 
-function onFocus() {
-  console.log('focus');
-}
+    function getDefaultValue() {    
+        if(props.priorOption !== undefined) {
+            return props.priorOption.label;
+        }
+        
+        //if(props.defaultValue !== undefined && props.datas.map((obj) => obj.label.toLowerCase()).includes(props.defaultValue.label.toLowerCase())) {
+        if(props.defaultValue !== undefined) {
+            return props.defaultValue.label;
+        }
+        return props.datas[0] && props.datas[0].label;
+    }
 
-function CustomSelect(props){
-    const [searchValue,setSearchValue] = useState(''); //数组解构search的状态
-    const [changeValue,setChangeValue] = useState(''); //数组解构change的状态
-    
     function onSearch(value) {  //search改变value
         console.log('search:', value);
-        setSearchValue(value);
-    }
-    function onChange(value) {  //change改变value
-        console.log('onChange:', value);
-        setChangeValue(value);
+        //如果用户设置了优先选项，并且输入的value和优先选项的值相同，则不设置uservalue
+        if(props.priorOption !== undefined && props.priorOption.label === value) {  
+            setUserValue('');
+            return;
+        }
+
+        //如果输入的value和源列表中的任何一项相同，则不设置uservalue
+        if(props.datas.map((obj) => obj.label.toLowerCase()).includes(value.toLowerCase())) {
+            setUserValue('');
+            return;
+        }
+
+        // 如果有用户自定义选项，并且输入的value与自定义选项相同，则不设置uservalue
+        if(userOption !==undefined && userOption.label === value) {
+            setUserValue('');
+            return;
+        }
+
+        setUserValue(value);
     }
 
-    let optionsValues = props.datas;  //定义存储options原始数据的变量
+    function onChange(value, option) {  //change改变value
+        console.log('onChange:', value ,option);
+        if(option === undefined) {
+            return;
+        }
 
-    if(searchValue !==''){
-        optionsValues = [searchValue,...optionsValues];  //optionsValues存储search的值+原始数据
+        if(userValue !== '' && option.label === userValue) {
+            setUserOption(option);
+            setUserValue('');
+        }else{
+            setUserOption();
+        }
+        props.onChange(value, option);//用户处理被选中值的回调函数
     }
 
-    if(changeValue !=='' && changeValue !== undefined){
-        optionsValues = [changeValue,...optionsValues];  //optionsValues存储change的值+原始数据
+    function onSelect(value, option) {
+        console.log('onSelect:', value , option);
+        setDefaultValue(value);
+    }
+
+    function onClear() {
+        console.log('onClear:');
+        setDefaultValue(getDefaultValue());
+    }
+
+    if(userOption !==undefined) { //如果用户选中的自定义option不为undefined，在原列表前添加
+        optionValues = [userOption, ...optionValues];
+    }
+
+    if(userValue !== '') {  //如果用户输入的值不为空，在原列表前添加
+        optionValues = [{value:userValue,label:userValue}, ...optionValues];
+    }
+
+    if(props.priorOption !== undefined) {   //设置优先选项，添加在列表第一项
+        optionValues = [props.priorOption, ...optionValues];
+    }
+    
+    //暂无数据效果
+    const notFoundContent = <div className="selectNodata">暂无数据</div>
+    // 自定义下拉内容loading状态
+    const loading = props.loading;
+    const dropdownRender = (Menu) => {
+        if(loading) {
+            return (
+            <>
+                <div className="selectLoading"><Spin /></div>
+            </>
+            );
+        }
+        return Menu;
     }
 
     return (
@@ -39,13 +101,19 @@ function CustomSelect(props){
             showSearch = {props.searchable}
             style={{ width: 200 }}
             placeholder={props.placeholder || '请选择默认'}
-            optionFilterProp="children"
+            value={defaultValue}
+            optionFilterProp="label"
+            // options={optionValues.map((obj) => {
+            //     return {value:obj.value,label:obj.label}
+            // })}
+            options={optionValues ? optionValues : notFoundContent}
             onChange={onChange}
-            onFocus={onFocus}
-            onBlur={onBlur}
+            onSelect={onSelect}
+            onClear={onClear}
+            loading
             onSearch={props.searchable ? onSearch : null}
             filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
             allowClear
             clearIcon={<i className='icon-input-close'></i>}
@@ -55,12 +123,16 @@ function CustomSelect(props){
                   <i className='icon-search'></i>
                 </>
             }
+            notFoundContent={notFoundContent}
+            dropdownRender={dropdownRender}
         >
-            {
-                Array.from(new Set(optionsValues)).map((value) => {   //Array.from(new Set(optionsValues))数组去重
-                    return <Option value={value} key={value}>{value}</Option>
+           {/* {
+                optionValues.map((obj) => {
+                    return <Option value={obj.value} key={obj.value} label={obj.label}>{obj.label}</Option>
                 })
-            }
+           }  */}
+           {/* <OptiongGroup optiongGroup={props.optiongGroup} /> */}
+
         </Select>
     )
 }
